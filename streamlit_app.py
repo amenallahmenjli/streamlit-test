f7bee8bf1524d2af089148dd28c1e629866b21bf
أمان الله منجلي <menjliamanullah@gmail.com>
2022-11-10 19:57:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-
+"""
 from diffusers import StableDiffusionPipeline
 import torch
 
@@ -9,4 +9,19 @@ pipe = pipe.to("cuda")
 prompt = "a photo of an astronaut riding a horse on mars"
 image = pipe(prompt).images[0]  
     
-image.save("astronaut_rides_horse.png")
+image.save("astronaut_rides_horse.png")"""
+
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
